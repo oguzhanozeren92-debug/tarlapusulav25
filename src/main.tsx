976ec0efa4f0c578feca23bbe757d0e8a12df7cb
oguzhanozeren92-debug <oguzhanozeren92@gmail.com>
2026-09-22 21:41:14 +0300

    feat: mount pinned crop suitability notification

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import PinnedCropSuitabilityNotification from './features/notifications/components/PinnedCropSuitabilityNotification';
 import './styles/TarlaPusulaTheme.css';
 import './styles/MobileAppShell.css';
 import './styles/WhiteAppTheme.css';
@@ -11,5 +12,6 @@ import './styles/MapReadabilityFix.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <PinnedCropSuitabilityNotification />
   </StrictMode>,
 )
